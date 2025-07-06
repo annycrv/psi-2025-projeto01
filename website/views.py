5a929ce76320bd9b6185485f5103bd8e73d8e080
annycrv <annacaroline.8002@gmail.com>
2025-07-05 22:21:35 -0300

    Adicionando HTML

--- website/views.py
+++ website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index (request):
+    return render(request, "index.html")
+
+def equipe (request):
+    return render(request, "equipe.html")
+
+def sobre (request):
+    return render(request, "sobre.html")
