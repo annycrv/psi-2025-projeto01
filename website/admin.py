af225e1651b3909b4de1029c51a5b0471a79462c
annycrv <annacaroline.8002@gmail.com>
2025-09-18 08:23:56 -0300

    fixtures

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Ator, Elenco, Index, Sobre
+from .models import Ator, Elenco, Index, Sobre, Carrossel
 
 admin.site.register(Ator)
 admin.site.register(Elenco)
 admin.site.register(Index)
 admin.site.register(Sobre)
+admin.site.register(Carrossel)
--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ class Ator(models.Model):
     idade = models.CharField(max_length=20)
     personagem = models.CharField(max_length=80)
     nascimento = models.DateField()
-    imagem = models.ImageField(upload_to='imagens/')
+    imagem = models.ImageField(upload_to='imagens-ator/')
     conteudo = models.CharField(max_length=2000)
 
     def __str__(self):
@@ -16,11 +16,11 @@ class Ator(models.Model):
 
 
 class Elenco(models.Model):
-    imagem_e = models.ImageField(upload_to='imagens/')
+    imagem_e = models.ImageField(upload_to='imagens-elenco/')
 
 class Index(models.Model):
     texto = models.CharField(max_length=3000)
-    banner = models.ImageField(upload_to='imagens/')
+    banner = models.ImageField(upload_to='imagens-banner/')
 
     def __str__(self):
         return self.texto
@@ -28,3 +28,13 @@ class Index(models.Model):
 
 class Sobre(models.Model):
     texto = models.CharField(max_length=5000)
+
+class Carrossel(models.Model):
+    nome = models.CharField(max_length=50)
+    imagem = models.ImageField(upload_to='imagens-carrossel')
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name_plural = "Carrossel"
